e06c6e8b51095e2651382477f16322dd40e9670b
Daniel Kocsis <kocsis.daniel@gmail.com>
2018-07-10 20:23:56 +0200

    Fixing build issues

--- src/test/java/com/danielkocsis/database/action/monitor/DatabaseActionMonitorApplicationTests.java
+++ src/test/java/com/danielkocsis/database/action/monitor/DatabaseActionMonitorApplicationTests.java
@@ -3,10 +3,12 @@ package com.danielkocsis.database.action.monitor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("integrationtest")
 public class DatabaseActionMonitorApplicationTests {
 
   @Test
